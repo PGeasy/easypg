d99f8dd4e45b5d89ec35a1af08d86bc710385ddd
ankit18018 <akankit6366@gmail.com>
2022-03-21 21:16:06 +0530

    ii

--- app/src/main/java/com/example/googlesigninapi/RegisterPgPage.java
+++ app/src/main/java/com/example/googlesigninapi/RegisterPgPage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,9 @@ import java.util.ArrayList;
 public class RegisterPgPage extends AppCompatActivity {
     private Toast t;
     static ArrayList<String> arr;
+    private TextView et1, et2,et3,et4, et12, et5,et6,et7,et8,et9,et10,et11;
+    private RadioButton ac, wifi,geyser, food,parking,security;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +27,24 @@ public class RegisterPgPage extends AppCompatActivity {
         t = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
         Button submit = (Button) findViewById(R.id.submit);
 
-        TextView et1  = (TextView) findViewById(R.id.et1);
-        TextView et2  = (TextView) findViewById(R.id.et2);
-        TextView et3  = (TextView) findViewById(R.id.et3);
-        TextView et4  = (TextView) findViewById(R.id.et4);
-        TextView et5  = (TextView) findViewById(R.id.et5);
-        TextView et6  = (TextView) findViewById(R.id.et6);
-        TextView et7  = (TextView) findViewById(R.id.et7);
-        TextView et8  = (TextView) findViewById(R.id.et8);
-        TextView et9  = (TextView) findViewById(R.id.et9);
-        TextView et10  = (TextView) findViewById(R.id.et10);
-        TextView et11 = (TextView) findViewById(R.id.et11);
-        TextView et12  = (TextView) findViewById(R.id.et12);
+        et1  = (TextView) findViewById(R.id.et1);
+        et2  = (TextView) findViewById(R.id.et2);
+        et3  = (TextView) findViewById(R.id.et3);
+        et4  = (TextView) findViewById(R.id.et4);
+        et5  = (TextView) findViewById(R.id.et5);
+        et6  = (TextView) findViewById(R.id.et6);
+        et7  = (TextView) findViewById(R.id.et7);
+        et8  = (TextView) findViewById(R.id.et8);
+        et9  = (TextView) findViewById(R.id.et9);
+        et10  = (TextView) findViewById(R.id.et10);
+        et11 = (TextView) findViewById(R.id.et11);
+        et12  = (TextView) findViewById(R.id.et12);
+        ac= (RadioButton) findViewById(R.id.ac);
+        wifi= (RadioButton) findViewById(R.id.wifi);
+        parking= (RadioButton) findViewById(R.id.parking);
+        geyser= (RadioButton) findViewById(R.id.geyser);
+        security= (RadioButton) findViewById(R.id.security);
+        food= (RadioButton) findViewById(R.id.food);
 
 
         RegisterPgPage.arr = new ArrayList<String>();
